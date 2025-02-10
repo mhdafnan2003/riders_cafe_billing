e27d415e5f2d67d360c65951188cc2e4c45c66fe
mhdafnan2003 <mhdafnan2003@gmail.com>
2025-02-11 01:23:17 +0530

    dashborad name fetched

--- lib/Screen/Dashboard/dashboard.dart
+++ lib/Screen/Dashboard/dashboard.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:riders_cafe/Screen/Authentication/Login/loginscreen.dart';
+import 'package:riders_cafe/service/authservice.dart';
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
@@ -6,8 +9,11 @@ class DashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+          onPressed: () => Get.offAll(() => LoginScreen()),
+          child: Icon(Icons.logout)),
       body: Center(
-        child: Text('dashboard'),
+        child: Text('Welcome ${Get.find<AuthService>().getUser()?['name']}'),
       ),
     );
   }
